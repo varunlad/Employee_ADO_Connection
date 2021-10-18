c85cde18c336c4c7a529724311f92201e9e040f9
varun <varunlad5@gmail.com>
2021-10-18 15:08:14 +0530

    [Varun] Connection SQL with .NET using ADO

---
+++ Ado_Net_Demo/EmployeeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado_Net_Demo
+{
+  public  class EmployeeModel
+    {
+        //Getter and setter fields(present in db)
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public double PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+        public double Deductions { get; set; }
+        public double TaxablePay { get; set; }
+        public double BasicPay { get; set; }
+        public DateTime startDate { get; set; }
+    }
+}
---
+++ Ado_Net_Demo/EmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ado_Net_Demo
+{
+  public  class EmployeeRepository
+    {
+        //Connecting to Database
+        public static string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=payRollService058;Integrated Security=True";
+        //passing the string to sqlconnection to make connection 
+        SqlConnection sqlconnection = new SqlConnection(connectionString);
+        
+    }
+}
